8a31cb75e4f9388334d24cf19ff02b24ce49e8a5
shardul <shardulgawande27@gmail.com>
2023-05-08 13:21:51 +0530

    first commit

--- chatbud/settings.py
+++ chatbud/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'base.apps.BaseConfig',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -119,6 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+# STATIC_ROOT = 
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
