9facd05a4b57334bcc7e476956774e627df4d62c
Shardul Gawande <shardulgawaned27@gmail.com>
2023-10-07 21:26:09 +0530

    second commit

--- chatbud/settings.py
+++ chatbud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pu^-kx9enb@%&mos#$w5r$^zg*y!dn6(y20i2&&^x-#12$js^-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -134,3 +134,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+STATIC_URL="static/"
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
